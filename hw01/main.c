e3c3bf4fb9e5eef5e6861b27cb1a9070e2b224e6
Jakub Filipovký <filipj25@fit.cvut.cz>
2022-01-18 23:41:07 +0100

    hw01: Second Commit

--- hw01/main.c
+++ hw01/main.c
@@ -4,7 +4,7 @@
 int main() {
     printf("Velikost latky:\n");
     double l2, l1, prekryv;
-    if (scanf("%lf %lf", &l2, &l1) != 2 || l2 <= 0 || l1 <= 0 ) {
+    if (scanf("%lf %lf", &l1, &l2) != 2 || l2 <= 0 || l1 <= 0 ) {
         printf( "Nespravny vstup.\n" );
         return 1;
     }
@@ -25,24 +25,27 @@ int main() {
     if (prekryv >= l1 && prekryv >= l2) {printf( "Nelze vyrobit.\n" ); return 0;}
 
 
-    int a,b;
-    if (fmod(p2,l2) != 0){a = floor(p2/l2)+1;}
-    else {a = p2/l2;}
-    if (fmod(p1,l1) != 0) {b = floor(p1/l1)+1;}
-    else {b = p1/l1;}
+    int a =1,b = 1;
+    int a1 = 1,b1 = 1;
+    if (l2 < prekryv && l1 > prekryv && l2 == 1 )
+    {
+        while ((((l1 - prekryv) * (b - 1)) + l1) < p1-0.0001) { b++;}
+            printf("Pocet kusu latky: %d\n", b);
+            return 0;
+    }
+    if (l1 < prekryv && l2 > prekryv && l1 == 1 )
+    {
+        while ((((l2 - prekryv) * (b - 1)) + l1) < p1-0.0001) { b++;}
+        printf("Pocet kusu latky: %d\n", b);
+        return 0;
+    }
 
-    int a1,b1;
-    if (fmod(p2,l1) != 0) {a1 = floor(p2/l1)+1;}
-    else {a1 = p2/l1;}
-    if (fmod(p1,l2) != 0) {b1 = floor(p1/l2)+1;}
-    else {b1 = p1/l2;}
+    while ((((l2 - prekryv) * (a - 1)) + l2) < p2-0.0001) { a++; }
+    while ((((l1 - prekryv) * (b - 1)) + l1) < p1-0.0001) { b++; }
 
 
-   while ((((l2 - prekryv) * (a-1)) + l2) < p2-0.00000000001) {a++;}
-   while ((((l1 - prekryv) * (b-1)) + l1 )< p1 - 0.00000000001) {b++;}
-
-   while ((((l1 - prekryv) * (a1-1)) + l1 )< p2-0.0000000001) {a1++;}
-   while ((((l2 - prekryv) * (b1-1)) + l2 )< p1-0.0000000001) {b1++;}
+   while ((((l1 - prekryv) * (a1-1)) + l1 )< p2) {a1++;}
+   while ((((l2 - prekryv) * (b1-1)) + l2 )< p1) {b1++;}
 
 
    int n1 = a*b;
@@ -52,7 +55,7 @@ int main() {
     printf("Pocet kusu latky: %d\n", k);
     return 0;
 
-    
+
 
 
 
